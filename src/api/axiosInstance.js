f85f0a430d46bcd3f6372ddc280cca8c05d6053f
ahmet <ahmetylm327@gmail.com>
2026-04-04 04:17:06 +0300

    hata

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://loomix-xlp4.onrender.com",
+    baseURL: "https://loomix-xlp4.onrender.com/api",
     headers: {
         'Content-Type': 'application/json'
     }
